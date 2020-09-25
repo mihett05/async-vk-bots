a919e4db75644dc9b8b09c7c8e0329f3c411d8f4
mihett05 <mihett05@yandex.ru>
2020-09-25 21:10:19 +0500

    Updated controllers

--- async_vk_bots/ext/Controller.py
+++ async_vk_bots/ext/Controller.py
@@ -4,7 +4,17 @@ from asyncio import iscoroutinefunction
 from async_vk_bots.ext.DataModels.Message.Message import Message
 
 
-class Controller:
+class MetaController(type):
+    controllers = dict()
+
+    def __new__(mcs, name, bases, attrs):
+        cls = super(MetaController, mcs).__new__(mcs, name, bases, attrs)
+        if name != "Controller":
+            mcs.controllers[cls] = None
+        return cls
+
+
+class Controller(metaclass=MetaController):
     def __init__(self, api, event, data):
         self.api = api
         self.event = event
@@ -57,22 +67,32 @@ def _get_re_func(exp, find_type="fullmatch"):
         raise Exception(f"Unknown find_type: {find_type}")
 
 
-def controller(re_command, find_type="fullmatch", bot=None):
+def controller(re_command, find_type="fullmatch"):
     def decorator(controller_cls: type):
-        command_cls = type(f"_{controller_cls.__name__}Command", (_ControllerCommand,), {
-            "__init__": lambda self: _ControllerCommand.__init__(
-                self, _get_re_func(re_command, find_type), controller_cls
-            )
-        })
-        if bot:
-            bot.add_command(command_cls)
-        return command_cls
+        if issubclass(controller_cls, Controller):
+            MetaController.controllers[controller_cls] = _get_re_func(re_command, find_type)
+        return controller_cls
     return decorator
 
 
-def command(re_command, find_type="fullmatch", bot=None):
+def chat_controller(controller_cls):
+    if issubclass(controller_cls, Controller):
+        class __ChatController(controller_cls):
+            async def message(self):
+                if self.model.peer_id >= 2_000_000_000:
+                    await super().message()
+        if controller_cls in MetaController.controllers:
+            MetaController.controllers[__ChatController] = MetaController.controllers.pop(controller_cls)
+        return __ChatController
+    else:
+        raise Exception(
+            f"{controller_cls.__name__} must be subclass of Controller"
+        )
+
+
+def command(re_command, find_type="fullmatch"):
     def decorator(func):
-        @controller(re_command, find_type, bot)
+        @controller(re_command, find_type)
         class __Command(Controller):
             async def message(self):
                 if iscoroutinefunction(func):
